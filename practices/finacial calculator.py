8caee421d58422d7d79f7c00dfcc1dc67889a190
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-01-26 13:44:57 -0700

    changed stuff

---
+++ practices/finacial calculator.py
@@ -0,0 +1,13 @@
+# AP Finacial calculator
+
+income = float(input("what is your monthly income? "))
+
+rent = input("what is your monthly reant/mortage? ")
+
+utilities = input("what is your monthly utilities bill? ")
+
+groceries = input("what is your monthly groceries bill? ")
+
+transportaiton = input("what is your monthly transportation? ")
+
+print("your rent is" , "$" , rent , "and that is" (rent/income)*100 "of your income.")
