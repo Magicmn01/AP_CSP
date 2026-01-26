09d8e349ea6c6a63316ba233b773296674fb700e
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-01-26 13:29:28 -0700

    added code

---
+++ notes/integer_floats.py
@@ -0,0 +1,13 @@
+"""price = float(input("How much did it cost: $"))
+
+tax_rate = float(input("What precent is the tax: %"))/100
+
+total_price = price * (1 + tax_rate)
+
+print(round(total_price,2))"""
+
+apples = int(input("how many apples are there: "))
+
+friends = 70
+
+print("Each friend gets", apples//friends, "and I have" , apples%friends, "left over")
--- practices/first program.py
+++ practices/first program.py
@@ -1,2 +1,5 @@
 # AP First program
 
+name = input("what is your name? ")
+
+print("hello" , name )
