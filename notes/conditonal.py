989df09baf192b5e9655c3d844e34fe83ce706c5
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-01-29 13:42:31 -0700

    change

---
+++ notes/conditonal.py
@@ -0,0 +1,14 @@
+"""number = 99
+
+if number < 10 and number >= -9:
+    print(f"{number} is a single digit number")
+elif number < 100:
+    print(f"{number} is a double digit")
+else:
+    print(f"{number} is a double digit number")
+    print("....or is it?")
+    new_number = number*number
+    if new_number > 1000:
+        print (f"surpise numuber {new_number}")"""
+
+
