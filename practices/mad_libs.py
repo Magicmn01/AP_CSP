9184b65a155c47d562c37abb8aab3bdfc18a75ef
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-01-30 15:54:46 -0700

    yeah

--- practices/mad_libs.py
+++ practices/mad_libs.py
@@ -1,10 +1,10 @@
 # AP Mad Libs
-adjective=input("can you me a adjective? ").strip()
-noun = input("can you me a noun? ").strip().capitalize()
-verb = input("can you me a verb in past tense? ").strip()
-adverb = input("can you me a adverb? ").strip()
-another_AD = input("can you me another adjective? ").strip()
-another_noun = input("can you me another noun? ").strip().capitalize()
+adjective=input("can you give me a adjective? ").strip()
+noun = input("can you give me a animal? ").strip().capitalize()
+verb = input("can you give me a verb in past tense? ").strip()
+adverb = input("can you give me a adverb? ").strip()
+another_AD = input("can you give me another adjective? ").strip()
+another_noun = input("can you give me another noun? ").strip().capitalize()
 
-sentence = "Today I went to the zoo. I saw a(n) " + adjective + noun + "jumping up and down in its tree. He " + verb + adverb + "through the large tunnel that led to its " + another_AD + another_noun + "."
+sentence = "Today I went to the zoo. I saw a(n) " + adjective + " " + noun + " jumping up and down in its tree. He " + adverb + " " + verb + " through the large tunnel that led to its " + another_AD + " " + another_noun + "."
 print(sentence)
