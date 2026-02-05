3dad3af30bbe3a9207e19e99f56020d9d34a9873
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-02-05 13:44:49 -0700

    almost done

--- practices/finacial calculator.py
+++ practices/finacial calculator.py
@@ -1,19 +1,21 @@
 # AP Finacial calculator
+def monthly(bill):
+     return float(input(f"what is your monthly {bill}"))
+income = monthly("income: ")
 
-income = float(input("what is your monthly income? "))
+rent = monthly("rent: ")
 
-rent = float(input("what is your monthly reant/mortage? "))
+utilities = monthly("utilities: ")
 
-utilities = float(input("what is your monthly utilities bill? "))
+groceries = monthly("groceries: ")
 
-groceries = float(input("what is your monthly groceries bill? "))
-
-transportaiton = float(input("what is your monthly transportation? "))
+transportaiton = monthly("transportaiton: ")
 
 saving = round(income*0.15 , 2)
-
-print("your rent is $" , rent , "and that is" , (round((rent/income)*100,2)) , "%" , "of your income.")
-print("your utilities is" , "$" , utilities , "and this is", (round((utilities/income)*100,2)) , "%" , "of your income.")
+def percent(bills):
+     return round(({bills}/income)*100,2)
+print("your rent is $" , rent , "and that is" , percent(rent) , "%" , "of your income.")
+print("your utilities is" , "$" , utilities , "and this is", percent(utilities) , "%" , "of your income.")
 print("your groceries is" , "$" , groceries , "and that is", (round((groceries/income)*100,2)) , "%" , "of your income.")
 print("your transportsaiton is" , "$" , transportaiton , "and that is", (round((transportaiton/income)*100,2)) , "%" , "of your income.")
 print("you should be saving" , "$" , saving , "that is" , "15" , "%" , "of your income")
