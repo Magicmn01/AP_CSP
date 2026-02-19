f66de79fcb9612b7d93ffc54616f95eb442a4f7c
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-02-19 13:31:56 -0700

    C

--- C/Notes/Varibles.c
+++ C/Notes/Varibles.c
@@ -3,13 +3,23 @@
 
 int main(){
     int number = 8;
-    float pi = 3.14;
-    char grade = 'a'; //single for single character
+    const float pi = 3.14;
+    char grade = 'A'; //single for single character
     char name[] = "jake"; //string use [] after naming variable
+    char user[50];
+
+    printf("Tell me your name\n");
+    scanf("%s", &user);
+
+
+    printf("How old are you:\n");
+    scanf("%d", &number);
+
+
 
     printf("working!\n");
     printf("%d\n", number);
     printf("the digit of pi are: %f\n", pi);
-    printf("your name is %s you are %d old. you have an %c in programming. Pi is %f\n", name, number, grade, pi);
+    printf("Your name is %s you are %d years old. You have an %c in programming. Pi is %f\n", user, number, grade, pi);
     return 0;
 }
---
+++ C/practices/First program.c
@@ -0,0 +1,17 @@
+//AP 6 first C program
+#include <stdio.h>
+
+
+int main(){
+   char user[50];
+
+
+    printf("Tell me your name\n");
+    scanf("%s", &user);
+    printf("Your name is %s", user);
+    return 0;
+
+
+
+
+}
