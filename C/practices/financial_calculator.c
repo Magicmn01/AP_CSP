e62aa5b287ab5ea4dea7fe033cac65bf6655f4fd
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-02-23 13:43:41 -0700

    almost

---
+++ C/practices/financial_calculator.c
@@ -0,0 +1,35 @@
+//AP 6 financial calculator
+#include <stdio.h> 
+
+int main(){
+    float income;
+    float rent;
+    float util;
+    float grocer;
+    float transport;
+   
+    printf("Tell me your monthly income\n");
+    scanf("%f", &income);
+    
+    printf("Tell me your monthly rent\n");
+    scanf("%f", &rent);
+    
+    printf("Tell me your monthly utilities\n");
+    scanf("%f", &util);
+
+    printf("Tell me your monthly groceries\n");
+    scanf("%f", &grocer);
+
+    printf("Tell me your monthly transportation\n");
+    scanf("%f", &transport);
+
+    printf("your rent is %.2f$, and that is %.2f percent of your income\n", rent , 100*(rent/income));
+    printf("your utilities is %.2f$, and that is %.2f percent of your income\n", util , 100*(util/income));
+    printf("your groceries is %.2f$, and that is %.2f percent of your income\n", grocer , 100*(grocer/income));
+    printf("your transportation is %.2f$, and that is %.2f percent of your income\n", transport , 100*(transport/income));
+    printf("you have %.2f dolllars left \n", income-rent-util-grocer-transport);
+
+
+
+    return 0;
+}
