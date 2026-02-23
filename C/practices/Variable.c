76b1fef7c94d24a7accbaa64bc6cdced1c534ca3
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-02-23 13:16:28 -0700

    hi

--- C/practices/Variable.c
+++ C/practices/Variable.c
@@ -5,11 +5,15 @@
 int main(){
     char name[] = "jake";
     int number = 8;
-    char gpa[] = 3.6;
+    float gpa = 3.6;
     char breakfest[] = "cerel";
-    char gpa[] = 3.6;
-    char gpa[] = 3.6;
-
-
-
+    char color[] = "red";
+    char school[] = "UVU";
+    int year = 2026;
+    char eye[] = "brown";
+    int age = 16;
+    char subject[] = "math";
+    
+printf("your name is %s, yor favorite number is %d Your GPA is %.1f, you ate %s for breakfest your favorite color is %s you go to %s the year is %d your eyes are %s you are %d years old and your favorite subject is %s.\n", name, number, gpa, breakfest, color, school, year, eye, age, subject);
+return 0;
 }
