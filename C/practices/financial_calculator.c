90476d5c44f1662e069c44865aae49b74aa404cf
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-02-25 13:08:34 -0700

    calc

--- C/practices/financial_calculator.c
+++ C/practices/financial_calculator.c
@@ -7,7 +7,7 @@ int main(){
     float util;
     float grocer;
     float transport;
-   
+    float saving = income*.15;
     printf("Tell me your monthly income\n");
     scanf("%f", &income);
     
@@ -23,14 +23,11 @@ int main(){
     printf("Tell me your monthly transportation\n");
     scanf("%f", &transport);
 
-    printf("your rent is %.2f$, and that is %.2f percent of your income\n", rent , 100*(rent/income));
-    printf("your utilities is %.2f$, and that is %.2f percent of your income\n", util , 100*(util/income));
-    printf("your groceries is %.2f$, and that is %.2f percent of your income\n", grocer , 100*(grocer/income));
-    printf("your transportation is %.2f$, and that is %.2f percent of your income\n", transport , 100*(transport/income));
-    printf("you have %.2f dolllars left \n", income-rent-util-grocer-transport);
-    printf("you should save 15 percent");
-
-
-
+    printf("Your rent is %.2f$, and that is %.2f percent of your income\n", rent , 100*(rent/income));
+    printf("Your utilities is %.2f$, and that is %.2f percent of your income\n", util , 100*(util/income));
+    printf("Your groceries is %.2f$, and that is %.2f percent of your income\n", grocer , 100*(grocer/income));
+    printf("Your transportation is %.2f$, and that is %.2f percent of your income\n", transport , 100*(transport/income));
+    printf("You should save 15 percent of your income which is %.2f$\n", saving);
+    printf("You have %.2f dolllars left \n", income-saving-rent-util-grocer-transport);
     return 0;
 }
