5602d1be87049c952b8d4e8fcce2c96483717b8c
Magicmn01 <atticus.pereyra@ucas-edu.net>
2026-03-02 13:43:56 -0700

    x

---
+++ C/Notes/lists.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int grades[] = {99, 84, 65, 96, 81, 35};
+    printf("%d\n", grades[2]);
+    grades[2] = 85;
+    printf("%d\n", grades[2]);
+    grades[4] = NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ C/Notes/loops.c
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+ int main(){
+    int i = 1; 
+    while(i <= 10){
+        printf("%d\n", i);
+        i++; //increases i by 1
+    }
+
+    srand(time(NULL));
+    printf("%d\n", rand() % 11);
+    printf("%d\n", (rand() % 10) +1);
+    printf("%d\n", rand());
+
+    int goose = rand() % 10 + 1;
+    int count = 1; 
+    
+    while (count < goose){
+        printf("duck\n");
+        count++;
+    }
+    printf("GOOSE!");
+
+
+    int num = rand() % 20 + 1;
+
+    while (true){
+        int guess; 
+        printf("guess number between 1 and 20: ");
+        scanf("%d", &guess);
+        if(guess==num){
+            printf("you win!\n");
+            break;
+        }else if(guess > num){
+            printf("guess lower\n");
+        }else{
+            printf("gues higher\n");
+        }
+
+    }
+
+
+    return 0;
+ }
